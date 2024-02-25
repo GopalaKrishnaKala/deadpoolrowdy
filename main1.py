95d39dbd7d6aade2c45512a517ba10adf6397e6e
K S N S Gopala Krishna <gopalakrishna.kala@gmail.com>
2024-02-25 04:49:27 -0600

    Add files via upload

--- main1.py
+++ main1.py
@@ -15,7 +15,18 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 
 st.set_page_config(layout="wide",)
 
-visit_days = st.sidebar.number_input('Enter the number of days of recent data you want', min_value=2, value=3)
+st.sidebar.markdown("How many days are you visiting San Antonio?")
+
+days_visit = st.sidebar.number_input('Enter the number of days of recent data you want', min_value=2, value=3)
+
+st.sidebar.markdown("Which month are you planning to visit San Antonio?")
+
+month_visit = st.sidebar.selectbox('Select Month you are visiting', ( 'January', 'February',"March",'April', 'May', 'June',"July",'August', 'September', 'October',"November",'December')
+
+st.sidebar.markdown("From Airport are you planning to come to San Antonio?")
+
+entry_airport = st.sidebar.selectbox('Select Month you are visiting', ( 'AUS', 'BOS',"DFW",'FLL', 'IAH', 'JFK',"MIA",'SEA', 'SFO', 'SLC')
+
 
 
 
