a0756d68b4d9f20c6fffda3382a4d21d152163b8
K S N S Gopala Krishna <gopalakrishna.kala@gmail.com>
2024-02-25 05:21:35 -0600

    Add files via upload

--- main1.py
+++ main1.py
@@ -13,7 +13,7 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 
-st.set_page_config(layout="wide",)
+st.set_page_config(layout="wide")
 
 st.sidebar.markdown("How many days are you visiting San Antonio?")
 
@@ -28,46 +28,44 @@ st.sidebar.markdown("From Airport are you planning to come to San Antonio?")
 entry_airport = st.sidebar.selectbox('Select Month you are visiting', ( 'AUS', 'BOS',"DFW",'FLL', 'IAH', 'JFK',"MIA",'SEA', 'SFO', 'SLC')
 
 
-
-##df = yf.download(option,start= start_date,end= end_date, progress=False)
                                      
-if option == 'AUS':
+if entry_airport == 'AUS':
     df_aus = pd.read_csv("flight_data_and_price_history_AUS_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Austin to San Antonio</h2>", unsafe_allow_html=True)
     df_aus.head()                                     
-elif option == 'BOS':
+elif entry_airport == 'BOS':
     df_bos = pd.read_csv("flight_data_and_price_history_BOS_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Boston to San Antonio</h2>", unsafe_allow_html=True)
     df_bos.head()
-elif option == 'DFW':
+elif entry_airport == 'DFW':
     df_dfw = pd.read_csv("flight_data_and_price_history_DFW_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Dallas to San Antonio</h2>", unsafe_allow_html=True)
     df_dfw.head()
-elif option == 'FLL':
+elif entry_airport == 'FLL':
     df_fll = pd.read_csv("flight_data_and_price_history_FLL_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Florida to San Antonio</h2>", unsafe_allow_html=True)
     df_fll.head()
-elif option == 'IAH':
+elif entry_airport == 'IAH':
     df_iah = pd.read_csv("flight_data_and_price_history_IAH_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Houston to San Antonio</h2>", unsafe_allow_html=True)
     df_iah.head()
-elif option == 'JFK':
+elif entry_airport == 'JFK':
     df_jfk = pd.read_csv("flight_data_and_price_history_JFK_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of New York to San Antonio</h2>", unsafe_allow_html=True)
     df_jfk.head()
-elif option == 'MIA':
+elif entry_airport == 'MIA':
     df_mia = pd.read_csv("flight_data_and_price_history_MIA_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Miami to San Antonio</h2>", unsafe_allow_html=True)
     df_mia.head()
-elif option == 'SEA':
+elif entry_airport == 'SEA':
     df_sea = pd.read_csv("flight_data_and_price_history_SEA_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Seattle to San Antonio</h2>", unsafe_allow_html=True)
     df_sea.head()
-elif option == 'SFO':
+elif entry_airport == 'SFO':
     df_sfo = pd.read_csv("flight_data_and_price_history_SFO_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of San Fransisco to San Antonio</h2>", unsafe_allow_html=True)
     df_sfo.head()
-elif option == 'SLC':
+elif entry_airport == 'SLC':
     df_slc = pd.read_csv("flight_data_and_price_history_SLC_to_SAT.csv")
     st.markdown("<h2 style='text-align: center; color: black;'>Previous Data of Salt Lake City to San Antonio</h2>", unsafe_allow_html=True)
     df_slc.head()
